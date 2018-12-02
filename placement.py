b84a90b279b8bbc8a886ac871451dbe6ecf5fbbe
Thomas Culp <culp@college.harvard.edu>
2018-12-02 17:21:28 -0500

    added a bunch of comments

--- placement.py
+++ placement.py
@@ -11,12 +11,18 @@ import scipy.optimize
 def placement(density, dimensions, n, metric, one_d=False):
     if not one_d:
         n = 2 * n
+    # guess for where the routers should be placed (random)
     guess = np.random.rand(n)
+    # the domain is the unit box in R^n
     bnds = [(0., 1.) for i in range(n)]
     print(guess)
     print(bnds)
+    # eps is the step size the algorithm takes to estimate the jacobian. it needs to
+    # take a step at least the size of the grid length in order to see any
+    # change (any smaller and steps will be within the same box on the grid)
+    # the method for optimization was picked arbitrarily...
     return scipy.optimize.minimize(fun=metric, x0=guess, args=(density, dimensions, one_d),
-            bounds=bnds, method='TNC', options={'eps': 3*min(1./density.shape[0], 1./density.shape[1])})
+            bounds=bnds, method='TNC', options={'eps': 2*min(1./density.shape[0], 1./density.shape[1])})
 # 'L-BFGS-B'
 
 # assuming each router has the same amount of throughput, compute average
@@ -25,6 +31,9 @@ def placement(density, dimensions, n, metric, one_d=False):
 def throughput(positions, density, dimensions, one_d):
     # processing positions
     print(positions)
+    # putting the input through a rectifier to get valid numbers in (0, 1). in the
+    # case of one dimension, converting the one number to a coordinate along the
+    # perimeter of the room. for two dimensions, doing the expected thing...
     if one_d:
         positions = np.array([to_coords(x, density.shape) for x in [hard_sig(x) for x in positions]])
     else:
@@ -47,7 +56,11 @@ def throughput(positions, density, dimensions, one_d):
         dist[:, :, 0] *= dimensions[0] / density.shape[0]
         dist[:, :, 1] *= dimensions[1] / density.shape[1]
         power[i] = np.log10(norm(dist))
-    # taking the maximum over all the computed powers to get power received
+    # for each position in the density array, computing which router gives the
+    # most power. this determines which router that position will get data from.
+    # adding the density to counts so that average throughput for all devices
+    # connected to the router can be computed later.
+    # assignment keeps track of which position in the density grid gets which router
     power = np.array(power)
     counts = np.zeros(len(positions))
     assignment = np.zeros(density.shape).astype(int)
@@ -55,11 +68,16 @@ def throughput(positions, density, dimensions, one_d):
         for j in range(power.shape[2]):
             assignment[i,j] = np.argmax(power[:,i,j])
             counts[assignment[i,j]] += density[i,j]
-    # creating an array of
+    # the total throughput to each router is just normalized to 1, so taking the
+    # reciprocal of counts gives the percentage of throughput each
+    # device connected to that router receives
     bandwidths = 1 / counts
+    # this process puts the throuput each device in the grid receives into an array
+    # and then takes the median of that array.
     all_antennas_bandwidth = [[] for i in range(density.shape[0] * density.shape[1])]
     for i, x in np.ndenumerate(density):
         all_antennas_bandwidth[i[0] * density.shape[1] + i[1]] = np.full(x, bandwidths[assignment[i]])
+    # the result is negated so that scipy.optimize.minimize will find the maximum
     return -np.median(list(itertools.chain.from_iterable(all_antennas_bandwidth)))
 
 
@@ -68,6 +86,9 @@ def throughput(positions, density, dimensions, one_d):
 def power(positions, density, dimensions, one_d):
     # processing positions
     print(positions)
+    # putting the input through a rectifier to get valid numbers in (0, 1). in the
+    # case of one dimension, converting the one number to a coordinate along the
+    # perimeter of the room. for two dimensions, doing the expected thing...
     if one_d:
         positions = np.array([to_coords(x, density.shape) for x in [hard_sig(x) for x in positions]])
     else:
@@ -94,13 +115,23 @@ def power(positions, density, dimensions, one_d):
     max_power = power[0]
     for i in range(1, len(power)):
         max_power = np.maximum(max_power, power[i])
-    # creating an array of
+    # this process puts the throuput each device in the grid receives into an array
+    # and then takes the median of that array.
     all_antennas_power = [[] for i in range(density.shape[0] * density.shape[1])]
     for i, x in np.ndenumerate(density):
         all_antennas_power[i[0] * density.shape[1] + i[1]] = np.full(x, max_power[i])
+    # the result is negated so that scipy.optimize.minimize will find the maximum
     return -np.median(list(itertools.chain.from_iterable(all_antennas_power)))
 
 
+# the identity function for 0 < x < 1. otherwise this is saturated at 0 or 1
+# 1                                 -----------------------
+#                                 /
+#                               /
+#                             /
+#                           /
+# 0 -----------------------
+#                         0       1
 def hard_sig(x):
     if x > 1.:
         return 1.
@@ -109,12 +140,14 @@ def hard_sig(x):
     else:
         return x
 
-
+# computing the distance from the origin
 def norm(x):
     x2 = x ** 2
     return np.sqrt(x2[:, :, 0] + x2[:, :, 1])
 
 
+# converting a one-dimensional coordinate to a two dimensional coordinate along
+# the perimeter of the density grid.
 # u has already been normalized: u \in (0, 1)
 def to_coords(u, shape):
     p = 2 * (shape[0] + shape[1])

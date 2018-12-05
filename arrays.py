86c660475f3edfb713af5245bb4c8114b85d629c
Thomas Culp <culp@college.harvard.edu>
2018-12-04 20:42:05 -0500

    ready to run simulations

--- arrays.py
+++ arrays.py
@@ -1,40 +1,47 @@
 import numpy as np
 
-# uniform distribution - ideal where noise is not an issue
-x = np.ones((5, 10))
-# print(x)
+# defining the size of the kirkland dhall (both in grid size and meters, given
+# we're taking those units to be the exact same)
+s = (20, 40)
 
 # random clusters - brain break
-L = list()
+L1 = list()
 for i in range(5):
-    y = np.random.randint(8, size=(5, 10)).astype('uint8')
-    y = y.tolist()
-    L.append(y)
+    y = np.random.randint(8, size=s).astype('uint8')
+    L1.append(y)
 
-# print(L)
+random_clusters = L1
+#print(L)
 
 # future VR/AR - 5 times the bandwidth https://venturebeat.com/2017/05/06/the-bandwidth-problem-5-issues-the-vr-industry-must-resolve/
 L2 = list()
 for i in range(5):
-    z = np.random.randint(8, 45, size=(5, 10)).astype('uint8')
-    z = z.tolist()
+    z = np.random.randint(8, 45, size=s).astype('uint8')
     L2.append(z)
-# print(L2)
+
+future = L2
+#print(L2)
 
 # one strip of high usage - office hours
 L3 = list()
 for i in range(5):
-    a = np.random.randint(5, size=(5, 10)).astype('uint8')
+    a = np.random.randint(5, size=s).astype('uint8')
     a[i] =  8
-    a = a.tolist()
     L3.append(a)
-# print(L3)
+
+office_hours = L3
+#print(L3)
 
 # high uniform distribution - meal times
 L4 = list()
 for i in range(4, 9):
-    b = np.zeros((5, 10))
+    b = np.zeros(s)
     b.fill(i)
-    b = b.tolist()
     L4.append(b)
-# print(L4)
+
+uniform = L4
+#print(L4)
+
+#for l in [L1, L2, L3, L4]:
+#    for x in l:
+#        print(x)
--- simulate.py
+++ simulate.py
@@ -1,9 +1,10 @@
 import placement
+import arrays
 import numpy as np
 import pickle
 
 # repeat: number of times to run placement
-# n : nubmer of routers
+# n : number of routers
 # density: 2-d matrix of device placement
 # dimensions: 2-tuple which has size of room (meters)
 # metric: desired number to optimize
@@ -12,40 +13,44 @@ def optimal_placement(repeat, density, dimensions, n, metric, one_d):
     return min([placement.placement(density, dimensions, n, metric, one_d)
         for i in range(repeat)], key=lambda x: x.fun)
 
-samp_dist = np.array([[1, 1, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]])
+#samp_dist = np.array([[1, 1, 0],
+#    [0, 0, 0],
+#    [0, 0, 0],
+#    [0, 0, 0],
+#    [0, 0, 0],
+#    [1, 1, 1],
+#    [0, 0, 0],
+#    [0, 0, 0],
+#    [0, 0, 0],
+#    [0, 0, 0]])
 
-a = np.random.randint(2, size=(5,10)).astype('uint8')
-a[0] =  8
-print(a)
+#a = np.random.randint(2, size=(5,10)).astype('uint8')
+#a[0] =  8
+#print(a)
+#
+#print(optimal_placement(50, a, (4, 100), 2, placement.throughput, one_d=False))
 
-print(optimal_placement(50, a, (4, 100), 2, placement.throughput, one_d=False))
-
-# put list of distributions here:
-test_distributions = []
 # in the interest of time, the running of results can be shortened by removing
 # one of the things we are interested in (for example, removing power from the
 # list of metrics
-results = [[[[optimal_placement(50, a, (4, 100), routers, metric,
-    one_d) for a in test_distributions] for routers in range(2, 5)] for metric
-    in [placement.throughput, placement.power]] for one_d in [True, False]]
+def simulate(repeat, test_distributions):
+    return [[[[optimal_placement(repeat, a, arrays.s, routers, metric,
+        one_d) for a in test_distributions] for routers in range(2, 5)] for metric
+        in [placement.throughput, placement.power]] for one_d in [True, False]]
 
-# fixed router positions...
-positions = []
-# the routers are fixed along the side of a wall
-fixed_results = [[[[metric(pos, a, (4, 100), True)] for pos in positions] for a in test_distributions] 
-    for metric in [placement.throughput, placement.power]]
+random_clusters_results = simulate(10, arrays.random_clusters)
+future_results = simulate(10, arrays.future)
+office_hours_results = simulate(10, arrays.office_hours)
 
 with open('results.dat', 'wb') as f:
-    pickle.dump(results, f)
+    pickle.dump((random_clusters_results, future_results, office_hours_results), f)
+
+# fixed router positions...
+fixed_positions = []
+# the routers are fixed along the side of a wall
+#fixed_results = [[[[metric(pos, a, (4, 100), True)] for pos in positions] for a in test_distributions] 
+#    for metric in [placement.throughput, placement.power]]
+
 
 # with open('results.dat', 'rb') as f:
 #   loaded_results = pickle.load(f)
